911332f938d4f7426ef6bb206af44786fa6d74c9
Edson Ajeje <edson.ajeje@terra.com.br>
2021-01-28 18:37:20 -0300

    Isolado component QuizContainer

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/react-in-jsx-scope */
-import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import db from '../db.json';
@@ -12,17 +11,7 @@ import Widget from '../src/components/Widget';
 import Button from '../src/components/Button';
 import Input from '../src/components/Input';
 import QuizLogo from '../src/components/QuizLogo';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 10px
-  }
-`;
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
   const router = useRouter();
