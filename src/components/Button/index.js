e453cf35ff2f30c91adfd144bf8f75a7dee76fbd
Edson Ajeje <edson.ajeje@terra.com.br>
2021-01-28 17:56:26 -0300

    Implement component Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable linebreak-style */
+import styled from 'styled-components';
+
+const Button = styled.button`
+    display: flexbox;
+    margin: 15px auto;
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+    background-color: ${({ theme }) => theme.colors.mainBg};
+    color: white;
+    border-radius: 4px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 10px 0;
+    width: 100%;
+    font-weight: bold;
+    font-size: medium;
+    
+    &:enabled {
+        &:hover {
+            background-color: ${({ color }) => color};
+            cursor: pointer;
+        }
+    }
+`;
+
+export default Button;
